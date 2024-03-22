c6a95c8b8d35e9ad7821a6af312c6b35ed29c46c
Luka0328 <lukasimicb@gmail.com>
2024-03-22 11:46:52 +0100

    drugi-commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let listaDestinacija = document.getElementById("lista-destinacija");
+console.log(listaDestinacija);
+let naslov = document.getElementsByClassName("title");
+console.log(naslov);
+let element = document.getElementsByTagName("li");
+console.log(element);
+
+let wrap=document.querySelector("#wrapper");
+console.log(wrap);
+let destinacije = document.querySelectorAll(".naziv");
+console.log(destinacije);
+
+console.log("Parent mod liste destinacije je: ", listaDestinacija.parentNode); 
+//console.log("Child nodes liste destinacije su: ", listaDestinacija.childNodes);
+console.log("Child nodes liste destinacije su: ", listaDestinacija.children);
